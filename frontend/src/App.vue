8064764bedb8bcabfc00b33ee717864764fdd710
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-04-07 20:53:48 +0900

    add vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import RepoQuery from './components/RepoQuery.vue';
+</script>
+
+<template>
+  <main>
+    <RepoQuery></RepoQuery>
+  </main>
+</template>
---
+++ frontend/src/components/RepoQuery.vue
@@ -0,0 +1,13 @@
+<script setup>
+import RepoForm from './form/RepoForm.vue';
+</script>
+
+<script>
+
+</script>
+
+<template>
+<h1>
+    <RepoForm></RepoForm>
+</h1>
+</template>
