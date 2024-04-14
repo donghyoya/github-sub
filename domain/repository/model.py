15f487f224c492f22d343d750c09edc77fb57e29
unknown <dongwooya123@gmail.com>
2024-04-14 20:51:49 +0900

    setting repository model.py

---
+++ domain/repository/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import  Column, String, \
+PrimaryKeyConstraint, BigInteger, Text, \
+Integer, ForeignKey
+from sqlalchemy.orm import relationshipnal
+
+from default.config.dbconfig import Base
+
+
+class Repository(Base):
+    __tablename__ = 'Repository'
+    rid = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
+    language = Column(String(10))
+    connectCnt = Column(Integer)
+    repoName = Column(String(25))
+    guid = Column(BigInteger, ForeignKey('GithubUser.uid'))
+    github_user = relationship("GithubUser", back_populates="repositories")
