e5542ef30373869f383d5a08a76f9620f488263c
unknown <dongwooya123@gmail.com>
2024-04-14 20:50:22 +0900

    setting user model.py

---
+++ domain/user/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import  Column, String, \
+PrimaryKeyConstraint, BigInteger, Text, \
+ Integer
+from sqlalchemy.orm import relationshipnal
+
+from default.config.dbconfig import Base
+
+class GithubUser(Base):
+    __tablename__ = 'GithubUser'
+    uid = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
+    username = Column(String(255))
+    site = Column(String(255))
+    ConnectCnt = Column(Integer)
+    follower = Column(Integer)
+    following = Column(Integer)
