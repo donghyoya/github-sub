14d3dff57bb4f8d5193c432a0fb7d134b59b06f1
unknown <dongwooya123@gmail.com>
2024-04-15 18:46:25 +0900

    add user router.py

---
+++ domain/user/router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException, Body, Query
+from . import service, schema
+from sqlalchemy.orm import Session
+from typing import List
+from default.config import dbconfig
+
+router = APIRouter(
+    tags=["user"]
+)
+
+def get_db():
+    try:
+        db = dbconfig.SessionLocal()
+        yield db
+    finally:
+        db.close()
