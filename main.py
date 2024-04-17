b0967d614143c755242f59c1e7f48a709eae7d2c
unknown <dongwooya123@gmail.com>
2024-04-17 09:19:14 +0900

    add middleware 2 main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+
+#도메인 적용
 from domain.user import router as user_router
 from domain.chatgpt import router as chatgpt_router
 from domain.airesult import router as airesult_router
 from domain.repository import router as repo_router
 from domain.sourcecode import router as sourcecode_router
 
+#미들웨어 적용
+from default.middleware.coresmiddleware import setup_cors
 
 app = FastAPI()
+
+setup_cors(app)
 
 app.include_router(user_router,"/user")
 app.include_router(chatgpt_router,"/chatgpt")
