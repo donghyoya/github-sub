bc8d90e8963c32a15b03949ff4beb04b40f6eab5
donghyoya <dongwooya123@gmail.com>
2024-05-27 15:06:54 +0900

    fix aiconfig

--- default/config/aiconfig.py
+++ default/config/aiconfig.py
@@ -1,24 +1,16 @@
 from fastapi import FastAPI, Depends
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import openai
 from typing import ClassVar
 import os
 
 class AiConfig(BaseSettings):
 
-    openai_api_key: str = os.getenv("OPEN_API_KEY")
-    model: str = "gpt-3.5-turbo"
-    messages: list = []
+    model_config = SettingsConfigDict(env_file=".env")
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
-        extra = "ignore"  # 정의되지 않은 필드를 무시
-        fields = {
-            'openai_api_key': {
-                'env': 'OPENAI_API_KEY',
-            },
-        }
+    openai_api_key: str
+    model: str
+    messages: list = []
         
     _instance = None
     client: ClassVar[openai.OpenAI] = None
